1a51156190faf552cdda4bd9e69fe671a431e6e6
mtreehouse <sym3927@naver.com>
2023-07-23 23:05:32 +0900

    setup

---
+++ src/app/api/hello/route.js
@@ -0,0 +1,21 @@
+import { TuyaContext } from "@tuya/tuya-connector-nodejs";
+export async function GET(request) {
+  const context = new TuyaContext({
+    baseUrl: "https://openapi.tuyaus.com",
+    accessKey: "hn7v3uwaemcjarx7nvwd",
+    secretKey: "b7143a599d6741b89b926a8570b6a379",
+  });
+
+  const device_id = "eb04329be2f87ba866rdqn";
+  const commands = await context.request({
+    path: `/v1.0/iot-03/devices/${device_id}/commands`,
+    method: "POST",
+    body: { commands: [{ code: "switch", value: true }] },
+  });
+  if (!commands.success) {
+    new Error();
+  }
+  console.log("Execution result:", commands);
+
+  return new Response(commands);
+}
